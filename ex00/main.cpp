a63957f458adec82c00e796140d8b5a01b6a773b
tsi <timo42@proton.me>
2025-02-11 00:00:18 +0100

    Cleanup

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,7 +1,6 @@
 #include <cstdlib>
 #include <exception>
 #include <iostream>
-#include <stdexcept>
 
 #include "Ansi.hpp"
 #include "Bureaucrat.hpp"
@@ -10,114 +9,127 @@
 #include "Options.hpp"
 #include "Repr.hpp"
 
+void testJohn(Logger &log) {
+    log.info() << "Trying to initialize a new Bureaucrat with grade " << repr(148) << std::endl;
+    Bureaucrat john("John Smith", 148, log);
+    log.info() << "Return value of john.getName():          " << john.getName() << std::endl;
+    log.info() << "Return value of john.getGrade():         " << john.getGrade() << std::endl;
+    log.info() << "Insertion operator (<<) overload output: " << john << std::endl;
+
+    try {
+        log.info() << "Calling first john.incrementGrade()" << std::endl;
+        john.incrementGrade();
+        log.info() << "John now: " << john << std::endl << std::endl;
+    } catch (const Bureaucrat::GradeTooHighException &gradeTooHigh) {
+        Logger::lastInstance().error() << "Caught GradeTooHighException: " << ansi::red(gradeTooHigh.what()) << ", continuing anyway\n" << std::endl;
+    }
+
+    try {
+        log.info() << "Calling second john.incrementGrade()" << std::endl;
+        john.incrementGrade();
+        log.info() << "John now: " << john << std::endl << std::endl;
+    } catch (const Bureaucrat::GradeTooHighException &gradeTooHigh) {
+        Logger::lastInstance().error() << "Caught GradeTooHighException: " << ansi::red(gradeTooHigh.what()) << ", continuing anyway\n" << std::endl;
+    }
+
+    try {
+        log.info() << "Calling third john.incrementGrade()" << std::endl;
+        john.incrementGrade();
+        log.info() << "John now: " << john << std::endl << std::endl;
+    } catch (const Bureaucrat::GradeTooHighException &gradeTooHigh) {
+        Logger::lastInstance().error() << "Caught GradeTooHighException: " << ansi::red(gradeTooHigh.what()) << ", continuing anyway\n" << std::endl;
+    }
+
+    try {
+        log.info() << "Calling fourth john.incrementGrade()" << std::endl;
+        john.incrementGrade();
+        log.info() << "John now: " << john << std::endl << std::endl << std::endl;
+    } catch (const Bureaucrat::GradeTooHighException &gradeTooHigh) {
+        Logger::lastInstance().error() << "Caught GradeTooHighException: " << ansi::red(gradeTooHigh.what()) << ", continuing anyway\n\n" << std::endl;
+    }
+}
+
+void testJane(Logger &log) {
+    log.info() << "Trying to initialize a new Bureaucrat with grade " << repr(3) << std::endl;
+    Bureaucrat jane("Jane Smith", 3, log);
+    log.info() << "Return value of jane.getName():          " << jane.getName() << std::endl;
+    log.info() << "Return value of jane.getGrade():         " << jane.getGrade() << std::endl;
+    log.info() << "Insertion operator (<<) overload output: " << jane << std::endl;
+
+    try {
+        log.info() << "Calling first jane.decrementGrade()" << std::endl;
+        jane.decrementGrade();
+        log.info() << "John now: " << jane << std::endl << std::endl;
+    } catch (const Bureaucrat::GradeTooLowException &gradeTooLow) {
+        Logger::lastInstance().error() << "Caught GradeTooLowException: " << ansi::red(gradeTooLow.what()) << ", continuing anyway\n" << std::endl;
+    }
+
+    try {
+        log.info() << "Calling second jane.decrementGrade()" << std::endl;
+        jane.decrementGrade();
+        log.info() << "John now: " << jane << std::endl << std::endl;
+    } catch (const Bureaucrat::GradeTooLowException &gradeTooLow) {
+        Logger::lastInstance().error() << "Caught GradeTooLowException: " << ansi::red(gradeTooLow.what()) << ", continuing anyway\n" << std::endl;
+    }
+
+    try {
+        log.info() << "Calling third jane.decrementGrade()" << std::endl;
+        jane.decrementGrade();
+        log.info() << "John now: " << jane << std::endl << std::endl;
+    } catch (const Bureaucrat::GradeTooLowException &gradeTooLow) {
+        Logger::lastInstance().error() << "Caught GradeTooLowException: " << ansi::red(gradeTooLow.what()) << ", continuing anyway\n" << std::endl;
+    }
+
+    try {
+        log.info() << "Calling fourth jane.decrementGrade()" << std::endl;
+        jane.decrementGrade();
+        log.info() << "John now: " << jane << std::endl << std::endl << std::endl;
+    } catch (const Bureaucrat::GradeTooLowException &gradeTooLow) {
+        Logger::lastInstance().error() << "Caught GradeTooLowException: " << ansi::red(gradeTooLow.what()) << ", continuing anyway\n\n" << std::endl;
+    }
+}
+
+void testFailure1(Logger &log) {
+    try {
+        log.info() << "Trying to initialize a new Bureaucrat with grade " << repr(1000) << std::endl;
+        Bureaucrat failure("Failure Smith", 1000, log);
+        log.info() << "Return value of failure.getName():          " << failure.getName() << std::endl;
+        log.info() << "Return value of failure.getGrade():         " << failure.getGrade() << std::endl;
+        log.info() << "Insertion operator (<<) overload output: " << failure << std::endl;
+    } catch (const Bureaucrat::GradeTooHighException &gradeTooHigh) {
+        Logger::lastInstance().error() << "Caught GradeTooHighException: " << ansi::red(gradeTooHigh.what()) << std::endl << std::endl << std::endl;
+    } catch (const Bureaucrat::GradeTooLowException &gradeTooLow) {
+        Logger::lastInstance().error() << "Caught GradeTooLowException: " << ansi::red(gradeTooLow.what()) << std::endl << std::endl << std::endl;
+    }
+}
+
+void testFailure2(Logger &log) {
+    try {
+        log.info() << "Trying to initialize a new Bureaucrat with grade " << repr(0) << std::endl;
+        Bureaucrat failure("Failure Smith", 0, log);
+        log.info() << "Return value of failure.getName():          " << failure.getName() << std::endl;
+        log.info() << "Return value of failure.getGrade():         " << failure.getGrade() << std::endl;
+        log.info() << "Insertion operator (<<) overload output: " << failure << std::endl;
+    } catch (const Bureaucrat::GradeTooHighException &gradeTooHigh) {
+        Logger::lastInstance().error() << "Caught GradeTooHighException: " << ansi::red(gradeTooHigh.what()) << std::endl << std::endl << std::endl;
+    } catch (const Bureaucrat::GradeTooLowException &gradeTooLow) {
+        Logger::lastInstance().error() << "Caught GradeTooLowException: " << ansi::red(gradeTooLow.what()) << std::endl << std::endl << std::endl;
+    }
+}
+
 int main(int ac, char **av) {
     try {
         Options options(ac, av);
         if (options.printHelp || options.printVersion) {
-            std::cout << (options.printHelp ? Constants::helpText : "ex00 version " + Constants::programVersion) << std::endl;
+            std::cout << (options.printHelp ? Constants::helpText : "CPP05 ex00 version " + Constants::programVersion) << std::endl;
             return EXIT_SUCCESS;
         }
         Logger log(std::cerr, options.logLevel);
 
-        log.info() << "Trying to initialize a new Bureaucrat with grade " << repr(148) << std::endl;
-        Bureaucrat john("John Smith", 148, log);
-        log.info() << "Return value of john.getName():          " << john.getName() << std::endl;
-        log.info() << "Return value of john.getGrade():         " << john.getGrade() << std::endl;
-        log.info() << "Insertion operator (<<) overload result: " << john << std::endl;
-
-        try {
-            log.info() << "Calling first john.incrementGrade()" << std::endl;
-            john.incrementGrade();
-            log.info() << "John now: " << john << std::endl << std::endl;
-        } catch (const Bureaucrat::GradeTooHighException &gradeTooHigh) {
-            Logger::lastInstance().error() << "Caught GradeTooHighException: " << ansi::red(gradeTooHigh.what()) << ", continuing anyway\n" << std::endl;
-        }
-
-        try {
-            log.info() << "Calling second john.incrementGrade()" << std::endl;
-            john.incrementGrade();
-            log.info() << "John now: " << john << std::endl << std::endl;
-        } catch (const Bureaucrat::GradeTooHighException &gradeTooHigh) {
-            Logger::lastInstance().error() << "Caught GradeTooHighException: " << ansi::red(gradeTooHigh.what()) << ", continuing anyway\n" << std::endl;
-        }
-
-        try {
-            log.info() << "Calling third john.incrementGrade()" << std::endl;
-            john.incrementGrade();
-            log.info() << "John now: " << john << std::endl << std::endl;
-        } catch (const Bureaucrat::GradeTooHighException &gradeTooHigh) {
-            Logger::lastInstance().error() << "Caught GradeTooHighException: " << ansi::red(gradeTooHigh.what()) << ", continuing anyway\n" << std::endl;
-        }
-
-        try {
-            log.info() << "Calling fourth john.incrementGrade()" << std::endl;
-            john.incrementGrade();
-            log.info() << "John now: " << john << std::endl << std::endl << std::endl;
-        } catch (const Bureaucrat::GradeTooHighException &gradeTooHigh) {
-            Logger::lastInstance().error() << "Caught GradeTooHighException: " << ansi::red(gradeTooHigh.what()) << ", continuing anyway\n\n" << std::endl;
-        }
-
-        log.info() << "Trying to initialize a new Bureaucrat with grade " << repr(3) << std::endl;
-        Bureaucrat jane("Jane Smith", 3, log);
-        log.info() << "Return value of jane.getName():          " << jane.getName() << std::endl;
-        log.info() << "Return value of jane.getGrade():         " << jane.getGrade() << std::endl;
-        log.info() << "Insertion operator (<<) overload result: " << jane << std::endl;
-
-        try {
-            log.info() << "Calling first jane.decrementGrade()" << std::endl;
-            jane.decrementGrade();
-            log.info() << "John now: " << jane << std::endl << std::endl;
-        } catch (const Bureaucrat::GradeTooLowException &gradeTooLow) {
-            Logger::lastInstance().error() << "Caught GradeTooLowException: " << ansi::red(gradeTooLow.what()) << ", continuing anyway\n" << std::endl;
-        }
-
-        try {
-            log.info() << "Calling second jane.decrementGrade()" << std::endl;
-            jane.decrementGrade();
-            log.info() << "John now: " << jane << std::endl << std::endl;
-        } catch (const Bureaucrat::GradeTooLowException &gradeTooLow) {
-            Logger::lastInstance().error() << "Caught GradeTooLowException: " << ansi::red(gradeTooLow.what()) << ", continuing anyway\n" << std::endl;
-        }
-
-        try {
-            log.info() << "Calling third jane.decrementGrade()" << std::endl;
-            jane.decrementGrade();
-            log.info() << "John now: " << jane << std::endl << std::endl;
-        } catch (const Bureaucrat::GradeTooLowException &gradeTooLow) {
-            Logger::lastInstance().error() << "Caught GradeTooLowException: " << ansi::red(gradeTooLow.what()) << ", continuing anyway\n" << std::endl;
-        }
-
-        try {
-            log.info() << "Calling fourth jane.decrementGrade()" << std::endl;
-            jane.decrementGrade();
-            log.info() << "John now: " << jane << std::endl << std::endl << std::endl;
-        } catch (const Bureaucrat::GradeTooLowException &gradeTooLow) {
-            Logger::lastInstance().error() << "Caught GradeTooLowException: " << ansi::red(gradeTooLow.what()) << ", continuing anyway\n\n" << std::endl;
-        }
-
-        try {
-            log.info() << "Trying to initialize a new Bureaucrat with grade " << repr(1000) << std::endl;
-            Bureaucrat failure("Failure Smith", 1000, log);
-            log.info() << "Return value of failure.getName():          " << failure.getName() << std::endl;
-            log.info() << "Return value of failure.getGrade():         " << failure.getGrade() << std::endl;
-            log.info() << "Insertion operator (<<) overload result: " << failure << std::endl;
-        } catch (const Bureaucrat::GradeTooHighException &gradeTooHigh) {
-            Logger::lastInstance().error() << "Caught GradeTooHighException: " << ansi::red(gradeTooHigh.what()) << std::endl << std::endl << std::endl;
-        } catch (const Bureaucrat::GradeTooLowException &gradeTooLow) {
-            Logger::lastInstance().error() << "Caught GradeTooLowException: " << ansi::red(gradeTooLow.what()) << std::endl << std::endl << std::endl;
-        }
-
-        try {
-            log.info() << "Trying to initialize a new Bureaucrat with grade " << repr(0) << std::endl;
-            Bureaucrat failure("Failure Smith", 0, log);
-            log.info() << "Return value of failure.getName():          " << failure.getName() << std::endl;
-            log.info() << "Return value of failure.getGrade():         " << failure.getGrade() << std::endl;
-            log.info() << "Insertion operator (<<) overload result: " << failure << std::endl;
-        } catch (const Bureaucrat::GradeTooHighException &gradeTooHigh) {
-            Logger::lastInstance().error() << "Caught GradeTooHighException: " << ansi::red(gradeTooHigh.what()) << std::endl << std::endl << std::endl;
-        } catch (const Bureaucrat::GradeTooLowException &gradeTooLow) {
-            Logger::lastInstance().error() << "Caught GradeTooLowException: " << ansi::red(gradeTooLow.what()) << std::endl << std::endl << std::endl;
-        }
+        testJohn(log);
+        testJane(log);
+        testFailure1(log);
+        testFailure2(log);
 
         log.info() << "Exiting" << std::endl;
         return EXIT_SUCCESS;
